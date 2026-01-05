bfc1e9e20a4d110dd6b79f18ad35be21a4a5f24b
cartooh <cartooh@users.noreply.github.com>
2026-01-06 05:04:36 +0900

    feat: update toolbar icon SVG paths for improved visual representation

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -23,17 +23,17 @@ const Toolbar: React.FC<ToolbarProps> = ({ onCopyMarkdown, onCopyHtml, isFullWid
                 >
                     {isFullWidth ? (
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M4 14h6v6"/>
-                            <path d="M20 10h-6V4"/>
-                            <path d="M14 10l7-7"/>
-                            <path d="M3 21l7-7"/>
+                            <path d="M4 12h6"/>
+                            <path d="M7 9l3 3-3 3"/>
+                            <path d="M20 12h-6"/>
+                            <path d="M17 9l-3 3 3 3"/>
                         </svg>
                     ) : (
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M15 3h6v6"/>
-                            <path d="M9 21H3v-6"/>
-                            <path d="M21 3l-7 7"/>
-                            <path d="M3 21l7-7"/>
+                            <path d="M10 12H4"/>
+                            <path d="M7 9l-3 3 3 3"/>
+                            <path d="M14 12h6"/>
+                            <path d="M17 9l3 3-3 3"/>
                         </svg>
                     )}
                 </button>
